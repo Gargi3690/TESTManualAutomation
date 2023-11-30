941c0f8b8e4196cb412ef59f9996de38a2805e34
Gargi3690 <102368631+Gargi3690@users.noreply.github.com>
2023-11-30 12:07:41 +0530

    Second Commit

---
+++ Demo/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		
+	        int a = 10,b = 20;
+	        int c;
+	        c = a+b;
+	        System.out.println("Addtion of two numbers :" +c );
+	        
+	}
+}
